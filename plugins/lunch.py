5a4fecfff923b880b9949a1876a97b1add44c915
nasa9084 <nasa9084@users.noreply.github.com>
2017-07-11 13:07:31 +0900

    fix: join

--- plugins/lunch.py
+++ plugins/lunch.py
@@ -10,7 +10,7 @@ def list_lunch(message):
     if not lunch:
         message.reply('ランチリストがありません')
         return
-    message.reply(', \n'.join(lunch))
+    message.reply(', \n'.join([str(l) for l in lunch]))
 
 @listen_to('^\$lunch$')
 def choose_lunch(message):
